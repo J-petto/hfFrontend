0d33e2b6d76f3148ccfabe7bf6aa4928ca97cffb
황민지 <hminji4565@gmail.com>
2025-01-22 11:49:47 +0900

    테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
 
-  console.log(import.meta.env.VITE_CORE_FRONT_BASE_URL);
-  console.log(import.meta.env.VITE_CORE_API_BASE_URL);
+  console.log(import.meta.env.PROD);
+  console.log(import.meta.env.MODE);
 
   fetch(import.meta.env.VITE_CORE_API_BASE_URL + "/api")
     .then((data) => console.log(data));
